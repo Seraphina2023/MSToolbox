c3a4985d69059b6cc6cd08f4a3f7f6c8a828d62a
ruozhuliufeng <18852971173@63.com>
2022-04-20 12:50:38 +0800

    新增Redis操作处理

---
+++ ms-core-common/src/main/java/com/msop/core/common/exception/IdempotencyException.java
@@ -0,0 +1,14 @@
+package com.msop.core.common.exception;
+
+/**
+ * 幂等性异常
+ * @author ruozhuliufeng
+ */
+public class IdempotencyException extends RuntimeException{
+    private static final long serialVersionUID = 6610083281801529147L;
+
+    public IdempotencyException(String message){
+        super(message);
+    }
+}
+
---
+++ ms-core-common/src/main/java/com/msop/core/common/model/Result.java
@@ -0,0 +1,54 @@
+package com.msop.core.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 统一返回工具类
+ *
+ * @author ruozhuliufeng
+ * @date 2021-08-04
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result<T> implements Serializable {
+    private T datas;
+    private Integer code;
+    private String msg;
+
+    public static <T> Result<T> succeed(String msg) {
+        return of(null, CodeEnum.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> Result<T> succeed(T model, String msg) {
+        return of(model, CodeEnum.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> Result<T> succeed(T model) {
+        return of(model, CodeEnum.SUCCESS.getCode(), "");
+    }
+
+    public static <T> Result<T> succeed() {
+        return succeed("操作成功");
+    }
+
+    public static <T> Result<T> of(T datas, Integer code, String msg) {
+        return new Result<>(datas, code, msg);
+    }
+
+    public static <T> Result<T> failed(String msg) {
+        return of(null, CodeEnum.ERROR.getCode(), msg);
+    }
+
+    public static <T> Result<T> failed(T model, String msg) {
+        return of(model, CodeEnum.ERROR.getCode(), msg);
+    }
+
+    public static <T> Result<T> failed() {
+        return failed("操作失败");
+    }
+}
