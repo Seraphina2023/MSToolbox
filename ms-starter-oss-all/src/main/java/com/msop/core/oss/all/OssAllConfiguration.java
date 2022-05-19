dc942606b83207bfbcbb7644a3130fcced8143bc
ruozhuliufeng <18852971173@63.com>
2022-05-19 12:03:43 +0800

    精简项目结构，新增OSS与SMS全量配置

---
+++ ms-starter-oss-all/src/main/java/com/msop/core/oss/all/OssAllConfiguration.java
@@ -0,0 +1,12 @@
+package com.msop.core.oss.all;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OSS 配置类
+ *
+ * @author ruozhuliufeng
+ */
+@Configuration
+public class OssAllConfiguration {
+}
---
+++ ms-starter-sms-all/src/main/java/com/msop/core/sms/all/SmsAllConfiguration.java
@@ -0,0 +1,12 @@
+package com.msop.core.sms.all;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SMS 配置
+ *
+ * @author ruozhuliufeng
+ */
+@Configuration
+public class SmsAllConfiguration {
+}
