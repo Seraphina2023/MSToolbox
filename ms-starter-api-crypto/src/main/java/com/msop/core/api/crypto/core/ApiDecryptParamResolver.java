44d1a9fe068e2ab1d375b83886f46a0ae37db907
ruozhuliufeng <18852971173@63.com>
2022-07-07 18:36:11 +0800

    新增API加密工具模块

---
+++ ms-starter-api-crypto/src/main/java/com/msop/core/api/crypto/core/ApiDecryptParamResolver.java
@@ -0,0 +1,52 @@
+package com.msop.core.api.crypto.core;
+
+import com.msop.core.api.crypto.annotation.decrypt.ApiDecrypt;
+import com.msop.core.api.crypto.bean.CryptoInfoBean;
+import com.msop.core.api.crypto.config.ApiCryptoProperties;
+import com.msop.core.api.crypto.util.ApiCryptoUtil;
+import com.msop.core.tool.jackson.JsonUtil;
+import com.msop.core.tool.utils.Charsets;
+import com.msop.core.tool.utils.StringUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.lang.reflect.Parameter;
+
+/**
+ * param 参数解析
+ *
+ * @author ruozhuliufeng
+ */
+@RequiredArgsConstructor
+public class ApiDecryptParamResolver implements HandlerMethodArgumentResolver {
+    private final ApiCryptoProperties apiCryptoProperties;
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return AnnotatedElementUtils.hasAnnotation(parameter.getParameter(), ApiDecrypt.class);
+    }
+
+    @Nullable
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) throws Exception {
+        Parameter parameter = methodParameter.getParameter();
+        ApiDecrypt apiDecrypt = AnnotatedElementUtils.getMergedAnnotation(parameter, ApiDecrypt.class);
+        String text = nativeWebRequest.getParameter(apiCryptoProperties.getParamName());
+        if (StringUtil.isBlank(text)) {
+            return null;
+        }
+        CryptoInfoBean infoBean = new CryptoInfoBean(apiDecrypt.value(), apiDecrypt.secretKey());
+        byte[] textBytes = text.getBytes(Charsets.UTF_8);
+        byte[] decryptData = ApiCryptoUtil.decryptData(apiCryptoProperties, textBytes, infoBean);
+        return JsonUtil.readValue(decryptData, parameter.getType());
+    }
+}
---
+++ ms-starter-api-crypto/src/main/java/com/msop/core/api/crypto/enums/CryptoType.java
@@ -0,0 +1,23 @@
+package com.msop.core.api.crypto.enums;
+
+/**
+ * 加密方式
+ *
+ * @author ruozhuliufeng
+ */
+public enum CryptoType {
+    /**
+     * des
+     */
+    DES,
+
+    /**
+     * aes
+     */
+    AES,
+
+    /**
+     * rsa
+     */
+    RSA
+}
